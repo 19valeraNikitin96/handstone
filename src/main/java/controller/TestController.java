fb11c237848bdd7e466b3cc7093b544580aa6be4
JAVA2505 <carelcontrols@ukr.net>
2018-11-02 21:50:01 +0200

    hs-4

--- src/main/java/controller/TestController.java
+++ src/main/java/controller/TestController.java
@@ -3,12 +3,13 @@ package controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/test")
 public class TestController {
     @GetMapping
-    public String test() {
-        return "test";
+    public ModelAndView test() {
+        return new ModelAndView("test");
     }
 }
