f64f94a385c835869f1a55ba57c0a12df490ad3b
JAVA2505 <carelcontrols@ukr.net>
2018-11-09 20:15:56 +0200

    25,26: done

--- src/main/java/controller/HSController.java
+++ src/main/java/controller/HSController.java
@@ -1,10 +1,12 @@
 package controller;
 
 import entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import service.CardService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,12 +15,22 @@ import java.io.IOException;
 @Controller
 @RequestMapping("/hs")
 public class HSController {
+
+    private final CardService card;
+
+    @Autowired
+    public HSController(CardService card) {
+        this.card = card;
+    }
+
     @GetMapping
     public ModelAndView hs(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         User u = (User) req.getSession().getAttribute("user");
         if (u != null) {
             ModelAndView out = new ModelAndView("hs");
             out.addObject("u", u);
+            out.addObject("cards", card.getCardsFromJson(u.getDeck()));
+            out.addObject("deckQuantity", u.quantityOfCards());
             return out;
         } else {
             resp.sendRedirect("/");
---
+++ src/main/java/service/CardService.java
@@ -0,0 +1,31 @@
+package service;
+
+import com.google.gson.Gson;
+import dao.HCardDao;
+import entity.Card;
+import entity.Cards;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Service
+public class CardService {
+
+    private final HCardDao cardDao;
+
+    @Autowired
+    public CardService(HCardDao cardDao) {
+        this.cardDao = cardDao;
+    }
+
+    public List<Card> getCardsFromJson(String json) {
+        List<Card> out = new LinkedList<>();
+        Cards cards = new Gson().fromJson(json, Cards.class);
+        for (Integer id : cards.cards) {
+            out.add(cardDao.getById(id));
+        }
+        return out;
+    }
+}
