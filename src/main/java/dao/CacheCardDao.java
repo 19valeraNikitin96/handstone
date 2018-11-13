9551af21b96a765d5fdf8e06ec35e3fad032225f
JAVA2505 <carelcontrols@ukr.net>
2018-11-13 14:23:26 +0200

    hs-31) completed

--- src/main/java/dao/CacheCardDao.java
+++ src/main/java/dao/CacheCardDao.java
@@ -1,6 +1,9 @@
 package dao;
 
 import entity.Card;
+import hibernate.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -10,11 +13,21 @@ public class CacheCardDao implements CDaoInt{
 
     @Override
     public Card getById(int id) {
-        return null;
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session s = sf.openSession();
+        s.beginTransaction();
+        Card out = (Card) s.createQuery("FROM Card WHERE id='" + id + "'").uniqueResult();
+        s.getTransaction().commit();
+        s.close();
+        return out;
     }
 
     @Override
     public List<Card> get() {
-        return null;
+        SessionFactory sf = HibernateUtil.getSessionFactory();
+        Session s = sf.openSession();
+        List<Card> out = s.createQuery("FROM Card").list();
+        s.close();
+        return out;
     }
 }
--- src/main/java/dao/HCardDao.java
+++ src/main/java/dao/HCardDao.java
@@ -1,9 +1,6 @@
 package dao;
 
 import entity.Card;
-import hibernate.HibernateUtil;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 
 import java.util.List;
 
@@ -12,21 +9,23 @@ public class HCardDao implements CDaoInt {
 
     @Override
     public Card getById(int id) {
-        SessionFactory sf = HibernateUtil.getSessionFactory();
+        /*SessionFactory sf = HibernateUtil.getSessionFactory();
         Session s = sf.openSession();
         s.beginTransaction();
         Card out = (Card) s.createQuery("FROM Card WHERE id='" + id + "'").uniqueResult();
         s.getTransaction().commit();
         s.close();
-        return out;
+        return out;*/
+        return null;
     }
 
     @Override
     public List<Card> get() {
-        SessionFactory sf = HibernateUtil.getSessionFactory();
+        /*SessionFactory sf = HibernateUtil.getSessionFactory();
         Session s = sf.openSession();
         List<Card> out = s.createQuery("FROM Card").list();
         s.close();
-        return out;
+        return out;*/
+        return null;
     }
 }
