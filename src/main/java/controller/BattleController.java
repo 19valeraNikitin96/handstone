b3d707655f574bdf0f29b93503ad9d19dd307540
JAVA2505 <carelcontrols@ukr.net>
2018-11-13 14:46:16 +0200

    hs-32) completed hs-33) completed hs-34) completed

--- src/main/java/controller/BattleController.java
+++ src/main/java/controller/BattleController.java
@@ -1,6 +1,19 @@
 package controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/battle/wait")
 public class BattleController {
 
+    @GetMapping
+    public ModelAndView waiting() {
 
+        ModelAndView out = new ModelAndView("wait");
+        return out;
+
+    }
 }
